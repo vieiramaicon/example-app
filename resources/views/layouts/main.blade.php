8a743b987a795579a4a19455d60a7d1b16462c4a
Maicon V. de Oliveira <maicon.vdeo@gmail.com>
2022-08-22 10:11:02 -0300

    Aula 09 - adição de script de icons

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -49,5 +49,8 @@
         @yield('content')
         
         <footer>HDC Events &copy; 2022 </footer>
+
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> 
     </body>
 </html>
